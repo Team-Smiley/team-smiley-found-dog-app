9a102f05723a86bb39da18049db79b1a4d5126a9
RayMan504 <uraeusmillet@gmail.com>
2019-05-28 11:05:06 -0500

    "(server) install path. define output file in webpack config. chnage transformations to effect jsx files"

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
+const path = require('path');
+
+const SRC = path.join(__dirname, '/client/src');
+const DIST = path.join(__dirname, '/client/dist');
+
 module.exports = {
     //define entry point for webpack to start transforming
-    entry: `${__dirname}/app/index.js`,
+    entry: `${SRC}/index.jsx`,
     module: {
         //transformations to perform before reaching browser
         loaders: [{
-            //transformations performed on all js files
-            test: /\.js$/,
+            //transformations performed on all jsx files
+            test: /\.jsx$/,
             //ignore node_modules
             exclude: /node_modules/,
             //run babel loader on all js files
             loader: 'babel-loader'
         }]
-    } 
+    },
+    output: {
+        filename: 'bundle.js',
+
+    }
 };    
