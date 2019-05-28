9170120c8ff446d1f4d7c48ef8f7e6dc02f63257
RayMan504 <uraeusmillet@gmail.com>
2019-05-28 10:46:10 -0500

    "(server) define webpack transformations in loaders array for webpack config"

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    entry: `${__dirname}/app/index.js` 
+    //define entry point for webpack to start transforming
+    entry: `${__dirname}/app/index.js`,
+    module: {
+        //transformations to perform before reaching browser
+        loaders: [{
+            //transformations performed on all js files
+            test: /\.js$/,
+            //ignore node_modules
+            exclude: /node_modules/,
+            //run babel loader on all js files
+            loader: 'babel-loader'
+        }]
+    } 
 };    
