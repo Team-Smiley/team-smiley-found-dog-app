560ffdbf85d078f4167ee5324560d24b18df2c73
Trevor Scott <Trvr.Scott1@gmail.com>
2019-05-28 12:54:45 -0500

    'Added dummy data for testing'

---
+++ client/dummyData.js
@@ -0,0 +1,26 @@
+const animals = [
+  {
+    name: 'Zelda'
+    type: 'Dog'
+    age:  4
+    image: 'dummyImage1'
+  },
+  {
+    name: 'Troy'
+    type: 'Dog'
+    age:  5
+    image: 'dummyImage2'
+  },
+  {
+    name: 'Mittens'
+    type: 'Cat'
+    age:  2
+    image: 'dummyImage3'
+  },
+  {
+    name: 'George'
+    type: 'Ferret'
+    age:  8
+    image: 'dummyImage4'
+  },
+]
