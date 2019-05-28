faaf23b787ff125b03971539b44b08adfe074fe1
RayMan504 <uraeusmillet@gmail.com>
2019-05-28 10:41:04 -0500

    "(server) complete entry file. define entry point in webpack config file"

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
 //require react
+const React = require('react');
 //require react dom
+const ReactDOM = require('react-dom');
 //require app component
+const App = require('./components/App.jsx');
+
 //render app component
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,3 @@
-module.exports = {};    
+module.exports = {
+    entry: `${__dirname}/app/index.js` 
+};    
