bb086a474b08cc4e944f994cc087f982ae09120a
RayMan504 <uraeusmillet@gmail.com>
2019-05-29 15:52:50 -0500

    "(auth) pseudocode for hash method to hash passwords"

--- database-psql/index.js
+++ database-psql/index.js
@@ -1,10 +1,14 @@
 const { Client } = require('pg')
 const client = new Client()
-//require sequelize
 
 //require bcrypt
 const bcrypt = require('bcrypt');
 
-// setup helper function for hashing passwords with bcrypt 
+// setup helper function on user model for hashing passwords with bcrypt 
+User.hash = (password) => {
+    // implement genSalt method on bcrypt object. takes number of times to salt and callback
+        // implement hash method of bcrypt object. takes password, salt from genSalt callback, and a callback
+            //store hash in database 
+}
 
 await client.connect()
