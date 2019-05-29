e66ceaa37d52d18047ca78ac3d120eb1ab0b03bd
RayMan504 <uraeusmillet@gmail.com>
2019-05-29 15:28:33 -0500

    "(auth) npm installed bcrypt for passwording hashing. pseudocode helper function to hash passwords"

--- database-psql/index.js
+++ database-psql/index.js
@@ -1,4 +1,10 @@
 const { Client } = require('pg')
 const client = new Client()
+//require sequelize
+
+//require bcrypt
+const bcrypt = require('bcrypt');
+
+// setup helper function for hashing passwords with bcrypt 
 
 await client.connect()
