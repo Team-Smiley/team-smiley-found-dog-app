b9d0284bd21bc6b94481910c7eb5ee757642b515
RayMan504 <uraeusmillet@gmail.com>
2019-05-30 18:48:37 -0500

    "(auth) firebase google auth displays pop up on login click"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,13 +6,7 @@ import Login from '../../components/Login.jsx'
 import withFirebaseAuth from 'react-with-firebase-auth'
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import firebaseConfig from '../../src/firebaseConfig.js';
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-const firebaseAppAuth = firebaseApp.auth();
-const providers = {
-    googleProvider: new firebase.auth.GoogleAuthProvider(),
-};
+import {auth, provider} from './firebaseConfig.js';
 
 //create class app component
 class App extends React.Component {
@@ -26,67 +20,66 @@ class App extends React.Component {
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
    }
-   //handle signup
-//    handleSignup() {
-//     firebase.auth().signInWithPopup(providers).then(function(result) {
-//         // This gives you a Google Access Token. You can use it to access the Google API.
-//         var token = result.credential.accessToken;
-//         // The signed-in user info.
-//         var user = result.user;
-//         // ...
-//       }).catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // The email of the user's account used.
-//         var email = error.email;
-//         // The firebase.auth.AuthCredential type that was used.
-//         var credential = error.credential;
-//         // ...
-//       });
-//    }
+   
 login() {
-    // const providers = {
-    //     googleProvider: new firebase.auth.GoogleAuthProvider(),
-    // };
-    firebase.auth().signInWithPopup({
-        googleProvider: new firebase.auth.GoogleAuthProvider(),
-    }).then((result) => {
+    auth.signInWithPopup(provider).then((result) => {
       this.setState({
           user: result.user
       })
     })
   }
   logout() {
-    firebase.auth().signOut().then((result) => {
+    auth.signOut().then((result) => {
       this.setState({
         user: null
       })
     })
   }
+  componentDidMount() {
+    this.authListener();
+  }
+  authListener() {
+    auth.onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({
+          user
+        })
+      }
+    })
+  }
    //render html
    render() {
-      const {
-        user,
-        signOut,
-        signInWithGoogle,
-      } = this.props;
-       return (
-           <div>
-               <div>Hello Found Dog</div>
-               <Login handleSignUp={this.login} user={user} signOut={this.logout} signInWithGoogle={signInWithGoogle}/>
-           </div>
-       )
+     let authButton = this.state.user ? 
+      <button onClick={this.logout}>Log Out</button> : 
+      <button onClick={this.login}>Log In</button>
+     return (
+       <div>
+         {authButton}
+       </div>
+     ) 
    }
+  //  render() {
+  //     const {
+  //       user,
+  //       signOut,
+  //       signInWithGoogle,
+  //     } = this.props;
+  //      return (
+  //          <div>
+  //              <div>Hello Found Dog</div>
+  //              {user ? <Dashboard/> : <Login handleSignUp={this.login} user={user} signOut={this.logout} signInWithGoogle={signInWithGoogle}/>}
+  //          </div>
+  //      )
+  //  }
 }
 
 
 
 
-export default withFirebaseAuth({
-    providers,
-    firebaseAppAuth,
-})(App);
-//render app component
+// export default withFirebaseAuth({
+//     providers,
+//     firebaseAppAuth,
+// })(App);
+// //render app component
 ReactDOM.render(<App />, document.getElementById('app'));
 
