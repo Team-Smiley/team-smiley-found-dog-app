c315ee79f5d243ab675db9dd4b401f676e53eb19
Tyl3rM <Tyl3rM@gmail.com>
2019-05-31 15:03:59 -0500

    Added Login and SignUp modules, to be customized. basic functionality.

--- client/src/App/pages/SignUp/index.jsx
+++ client/src/App/pages/SignUp/index.jsx
@@ -1,13 +1,90 @@
-import React from 'react';
+import React from 'react'
+import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import withStyles from '@material-ui/core/styles/withStyles'
+import { Link, withRouter } from 'react-router-dom'
 
-// create component functio SignUp, for SignUp page, so it can be used with react hook
-function SignUp(props){
-    return(
-        <div>
-        SignUp Here!
-        </div>
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+})
+
+function SignUp(props) {
+    const { classes } = props
+
+    return (
+        <main className={classes.main}>
+            <Paper className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Register Account
+                </Typography>
+                <form className={classes.form}>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="name">Name</InputLabel>
+                        <Input id="name" name="name" autoComplete="off" autoFocus />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="email">Email Address</InputLabel>
+                        <Input id="email" name="email" autoComplete="off"  />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <Input name="password" type="password" id="password" autoComplete="off"  />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="fruit">Your Favorite Fruit</InputLabel>
+                        <Input name="fruit" type="text" id="fruit" autoComplete="off" />
+                    </FormControl>
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}>
+                        Register
+                    </Button>
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        className={classes.submit}>
+                        Go back to Login
+                    </Button>
+                </form>
+            </Paper>
+        </main>
     )
 }
 
-// export to be routed in main App 
-export default SignUp 
+export default withStyles(styles)(SignUp)
