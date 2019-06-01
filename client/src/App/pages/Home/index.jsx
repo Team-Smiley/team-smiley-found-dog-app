a97966e5ba00617c689fcf68b90b14f555704ca5
Tyl3rM <Tyl3rM@gmail.com>
2019-06-01 01:12:43 -0500

    Added basic styling layout for homepage.

--- client/src/App/pages/Home/index.jsx
+++ client/src/App/pages/Home/index.jsx
@@ -3,6 +3,35 @@ import { Typography, Paper, Avatar, Button} from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import withStyles from '@material-ui/core/styles/withStyles'
 
+// add basic styling wanted
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        display: 'block', 
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.primary.main,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+})
+
 // create component function Home, for HomePage, so it can be used with react hooks
 function Home(props){
     return(
@@ -14,4 +43,5 @@ function Home(props){
 }
 
 // export to be routed in main App 
-export default Home 
+// add style exporting by calling withStyles
+export default withStyles(styles)(HomePage)
