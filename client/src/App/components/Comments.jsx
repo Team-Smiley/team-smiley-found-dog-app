7542cc61d9e7405802576499e26d99bbf6124b25
RayMan504 <uraeusmillet@gmail.com>
2019-06-01 10:06:44 -0500

    "(comments) add button for adding comments. use axios post in click handler "

--- client/src/App/components/Comments.jsx
+++ client/src/App/components/Comments.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 // trigger post request on button click to store comments in database
-const 
+const addComment = (comment) => {
+    //axios post request to store comment in database
+    axios.post('/comments', {
+        comment
+    }).then((res) => {
+        //check route to see if server post request was triggered
+    })
+    //reset state by adding comment to array
+}
 //make handle change to bind comment input 
 function Comments(props) {
     return (
@@ -9,7 +17,17 @@ function Comments(props) {
             <label>Comment:
                 <input type="text" name="message" />
             </label>
-            <br/>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                // route login back to login
+                component={Link}
+                to="/signin"
+                className={classes.submit}>
+                Back to Sign-In
+            </Button>
         </div>
     )
 }
