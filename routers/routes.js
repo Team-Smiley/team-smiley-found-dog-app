e32187752ff6813c579f694b3298033fd89360a5
Christopher Huston <chusto2@gmail.com>
2019-06-01 18:57:08 -0500

    able to get pets back from database

--- routers/routes.js
+++ routers/routes.js
@@ -53,10 +53,10 @@ router.route('/user')
             //else
         //redirect to login
         Pets.findAll()
-            .then(pets => res.json(pets))
+            .then(pets => res.send(pets))
             .catch(err => console.error(err))
         
-        res.send('heres the pets')
+        // res.send('heres the pets')
     }).post((req, res, next) => {
         //add user comments to database
         console.log(req.body);
